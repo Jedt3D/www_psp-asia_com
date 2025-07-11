421bdddbf7a69ee54b73cc63b921de9557bf859a
Worajedt FourteenPro <sjedt@3ddaily.com>
2025-07-11 15:40:19 +0700

    fix the Sinatra app migration - css, js, img files linked correctly - test with just `/index` only
    #todo : add more routes

--- app.rb
+++ app.rb
@@ -1,6 +1,12 @@
 require 'sinatra'
 
+configure do
+  enable :static
+  set :public_folder, File.dirname(__FILE__) + '/public'
+  set :static_cache_control, [:public, max_age: 60 * 60 * 24]
+end
+
 get '/' do
-  'Hello, World!'
+  erb :index
 end
 
